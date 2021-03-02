189c0e29c44b225da6064a7716ea2cbd09eaf594
DaudHanafi <76601784+daudhanafi60@users.noreply.github.com>
2021-03-02 18:36:11 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,88 @@
+const help = (prefix) => {
+        return `
+╭────────────────
+⛶┃ *༺ *DAUD BOT*
+│╭────── 
+├╯
+├───╗ 
+│ ⛬ ╔╯ *VERSION : V 6.0.0*
+├──╯────────────────
+*┃⛨─✧ུNama :  DAUD BOT*
+*┃⛨─✧ུType : Eren Bot V6*
+*┃⛨─✧ུFounder : Arifi & Team*
+*┃⛨─✧ུStatus : ACTIVE*
+*┃⛨─✧ུOwner : Arifi Razzaq*
+*┃⛨─✧ུPrefix : ❪ ${prefix} ❫*
+*╰─╗*
+*┎─┸─⪼⦁ RULES*
+├➤ *TELP ✖️*
+├➤ *SPAM ✖️*
+├➤ *Langgar? BAN + BLOK*
+└───────────────╗
+╔───────────────╯
+├─ *❲ ⦁ APA YANG BARU ❳*
+│
+├◘ *Cek : ${prefix}cekup*
+├◘ *riwayat : ${prefix}memobot*
+├◘ *Semua Menu : ${prefix}all*
+├◘ *Thanx To : ${prefix}bycc*
+└───────────────
+
+ 
+┌──「 *LIST MENU* 」
+│
+├ ✎◞ *${prefix}ownermenu*
+├ ✎◞ *${prefix}adminmenu*
+├ ✎◞ *${prefix}vipmenu*
+├ ✎◞ *${prefix}mediamenu*
+├ ✎◞ *${prefix}kerangmenu*
+├ ✎◞ *${prefix}makemenu*
+├ ✎◞ *${prefix}othermenu*
+├ ✎◞ *${prefix}animemenu*
+├ ✎◞ *${prefix}nsfwmenu*
+├ ✎◞ *${prefix}funmenu*
+├ ✎◞ *${prefix}wibumenu*
+├ ✎◞ *${prefix}soundmenu*
+└ ✎◞ *${prefix}hackingmenu*
+
+
+┌──「 *AKSESBILITAS* 」
+│
+├ ✎◞ *${prefix}leveling [Enable/Disable]*
+├ ✎◞ *${prefix}Anime [1/0]*
+├ ✎◞ *${prefix}nsfw [1/0]*
+├ ✎◞ *${prefix}welcome [1/0]*
+├ ✎◞ *${prefix}public [1/0]*
+├ ✎◞ *${prefix}event [1/0]*
+└ ✎◞ *${prefix}antilinkgroup [1/0]* 
+
+
+┌──「 *DAUD HANAFI* 」
+│
+├ ✎◞ *${prefix}developer*
+├ ✎◞ *${prefix}jadiselebgram*
+├ ✎◞ *${prefix}daftarmemberselebgram*
+└ ✎◞ *${prefix}jasahapusakun*
+
+
+
+        ▉║█▐▉▉▐▐▍█║▍▉▏▍▍
+        ▉║█▐▉▉▐▐▍█║▍▉▏▍▍   
+                                        
+                               
+          *© DAUD HANAFI*
+          
+          
+          
+┎── *「 MY BOT TEAM」*
+*│*
+*├➤ _-DAUD HANAFI-_*
+*├➤ _-AHMAD-_*
+*├➤ _-MY DOI :V-_'*
+                                                        
+                                                                                                                                                    
+*Jangan Lupa Untuk Donasi Agar Bot Selalu Aktif*
+https://saweria.co/Arifirazzaq2001
+`
+}
+exports.help = help
